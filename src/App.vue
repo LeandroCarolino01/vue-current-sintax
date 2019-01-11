2ddf41c4973188c2ffa502405969748f91499bbd
Leandro.Carolino <carolino.leandro@gmail.com>
2019-01-11 22:52:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+   <Todos v-bind:todos="todos" />
+   
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos';
+
+
+export default {
+  name: 'app',
+  components: {
+   Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id:1,
+          title: "todo one",
+          completed: false
+        },
+        {
+          id:2,
+          title: "todo two",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica;
+  line-height: 1.4;
+}
+</style>
