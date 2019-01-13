31ae4376bb14eb9f6281b4a24e84a7152b4bd852
Leandro.Carolino <carolino.leandro@gmail.com>
2019-01-13 21:08:39 +0100

    delete func added

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 
 
@@ -28,14 +29,26 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err));
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+        .then(res => this.todos = this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+      
     }
   },
   created() {
-    
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
   }
 }
 </script>
